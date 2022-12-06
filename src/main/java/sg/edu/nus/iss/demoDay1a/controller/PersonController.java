b14b075c61d1d52eb3e29b56d69b3a1a90e4c672
sawarntminzaw <e1045759@u.nus.edu>
2022-12-06 16:24:32 +0800

    delete person

--- src/main/java/sg/edu/nus/iss/demoDay1a/controller/PersonController.java
+++ src/main/java/sg/edu/nus/iss/demoDay1a/controller/PersonController.java
@@ -63,4 +63,9 @@ public class PersonController {
         personService.updatePerson(p);
         return "redirect:/persons/list";
     }
+    @PostMapping(value = "/deletePerson")
+    public String deletePerson(@ModelAttribute(value = "person") Person p){
+        personService.removePerson(p);
+        return "redirect:/persons/list";
+    }
 }
